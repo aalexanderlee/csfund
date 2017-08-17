fc68f12164fb5ec2811bac59070581b690db6692
Austin Alexander Lee <austin.alexander.lee@gmail.com>
2017-08-17 11:03:19 -0700

    adding insertionSort js version 1

---
+++ insertionSort/insertionSort.js
@@ -0,0 +1,41 @@
+var arraySize = 40;
+var array = [];
+
+for (var index = 0; index < arraySize; index++) {
+  var randomNumber = Math.round(Math.random() * arraySize);
+  array.push(randomNumber);
+}
+
+function swap(items, firstIndex, secondIndex) {
+  var temp = items[firstIndex];
+  items[firstIndex] = items[secondIndex];
+  items[secondIndex] = temp;
+}
+
+function insertionSort(items) {
+  //var len = items.length;
+  var min;
+  for (var i = 0; i < items.length; i++) {
+    min = i;
+
+    for (var j = i + 1; j < items.length; j++) {
+      if (items[j] < items[min]) {
+        swap(items, i, min);
+        //min = j;
+      } else if (i!==min) {
+        swap(items, i, min);
+    }
+
+    // if (i !== min) {
+    //   swap(items, i, min);
+    // }
+    //
+  }
+  return items;
+}
+
+// console.log("PRE-SORT");
+// console.log(array.join(" "));
+// console.log("---------------------------");
+// console.log("POST-SORT");
+// console.log(insertionSort(array).join(" "));
